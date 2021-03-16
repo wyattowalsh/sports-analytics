f72b6ef7120c2319154ea68f46cbac1e57cdea25
Wyatt <wyattowalsh@gmail.com>
2021-03-15 17:40:50 -0700

    adding bash script to install miniconda in google colab

---
+++ project_resources/bash_scripts/install_conda_in_colab.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python_version = "$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')"
+
+MINICONDA_INSTALLER_SCRIPT=Miniconda3-4.5.4-Linux-x86_64.sh
+MINICONDA_PREFIX=/usr/local
+wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
+chmod +x $MINICONDA_INSTALLER_SCRIPT
+./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX
+
+if [[ "$(which conda)" =~ "/usr/local/bin/conda" ]]; then
+	conda install --channel defaults conda python="$python_version" --yes
+	conda update --channel defaults --all --yes
+	python -c 'import sys; _ = (sys.path.append("/usr/local/lib/python3.6/site-packages"))'
