343448c7e610b89908e895b4661b85f6bb82c497
Wyatt <wyattowalsh@gmail.com>
2021-03-21 20:23:23 -0700

    dependencies

--- project_resources/bash_scripts/install_conda_in_colab.sh
+++ project_resources/bash_scripts/install_conda_in_colab.sh
@@ -12,6 +12,7 @@ if [[ "$(which conda)" =~ "/usr/local/bin/conda" ]]; then
 	conda install --channel defaults conda python="$python_version" --yes
 	conda update --channel defaults --all --yes
 	python -c 'import sys; version = sys.version_info[0:2]; version = "{}.{}".format(version[0], version[1]); _ = (sys.path.append("/usr/local/lib/python{}/site-packages".format(version)))'
+	rm Miniconda3-4.5.4-Linux-x86_64.sh
 else
 	echo "conda installation failed. Colab has changed since the authoring of this script."
 fi
